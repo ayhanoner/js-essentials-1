0eaf06b4240edd4487aac8950d0d3b6bd96ae030
ayhanoner <ayhannoner12@gmail.com>
2026-08-12 02:36:18 +0300

    practice3

--- sampleFolder/calculateArea/calculate_Area.js
+++ sampleFolder/calculateArea/calculate_Area.js
@@ -8,3 +8,16 @@ function calculateArea(){
     let area = length * width;
     document.getElementById("result").innerHTML = `The area of the rectangle is: ${area}`;
 }
+
+let grocery1;
+let grocery2;
+let grocery3;
+
+function calculateGroceryTotal(){
+    grocery1 = parseFloat(document.getElementById("grocery1").value);
+    grocery2 = parseFloat(document.getElementById("grocery2").value);
+    grocery3 = parseFloat(document.getElementById("grocery3").value);
+
+    let total = grocery1 + grocery2 + grocery3;
+    document.getElementById("groceryResult").innerHTML = `The total amount of groceries is: ${total}`;
+}
