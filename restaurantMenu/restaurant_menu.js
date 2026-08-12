0cf934d8e6853fe7db5a84d5d244456ce0bf3932
ayhanoner <ayhannoner12@gmail.com>
2026-08-13 02:48:16 +0300

    arrpra1

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes - $5.99', 'Eggs Benedict - $8.99', 'Oatmeal - $3.99', 'Frittata - $7.99'];
+const mainCourseMenu = ['Steak - $15.99', 'Pasta - $12.99', 'Burger - $10.99', 'Salmon - $14.99'];
+const dessertMenu = ['Cake - $4.99', 'Ice Cream - $3.99', 'Pudding - $3.99', 'Fruit Salad - $5.99'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item,index) => `<p>Item ${index + 1}: ${item} </p>`).join('');
         document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
